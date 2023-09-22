23a9eaa31669b6e0ed31b380217325e2a6addaa2
Damoke01 <okedamola01@gmail.com>
2023-09-22 21:05:19 +0000

    portfolio title and favicon update

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,7 +1,3 @@
-import sharinKaka from '../assets/sharinKaka.jpg';
-import Ragnar from '../assets/Ragnar.jpg';
-import AOTscouts from '../assets/AOTscouts.png';
-import sasuke from '../assets/sasuke.jpg'
 import acmeSite from '../assets/acmeSite.jpg';
 
 export const ProjectList = [
@@ -9,21 +5,7 @@ export const ProjectList = [
         name: "Acme Rockets site clone",
         image: acmeSite,
         skills: "HTML, TailwindCSS & JS",
-        link: "https://flyacme.netlify.app"
-    },
-    {
-        name: "King Ragnar",
-        image: Ragnar,
-        skills: "HTML, CSS, JAVASCRIPT"
-    },
-    {
-        name: "Uchiha Sasuke",
-        image: sasuke,
-        skills: "HTML, CSS, JAVASCRIPT"
-    },
-    {
-        name: "Attack on titan scouts regiment crest",
-        image: AOTscouts,
-        skills: "HTML, CSS, JAVASCRIPT"
+        link1: "https://github.com/Okedamola01/AcmeRockets",
+        link2: "https://flyacme.netlify.app"
     },
 ];
--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -13,8 +13,10 @@ const ProjectPage = () => {
         <h1>{project.name}</h1>
         <img src={project.image} alt=''/>
         <p> <b>Skills</b>: {project.skills} </p>
-        <GitHubIcon />
-        <a href={project.link} target="_blank" rel="noopener noreferrer">
+        <a href={project.link1} target="_blank" rel="noopener noreferrer">
+          <GitHubIcon />
+        </a>
+        <a href={project.link2} target="_blank" rel="noopener noreferrer">
           <LinkIcon />
         </a>
     </div>
